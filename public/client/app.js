fbcc6639dd77edb02ca1b57f2d9cb1fad4dede65
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-02-08 16:54:11 -0800

    Enables adding new link to database

--- public/client/app.js
+++ public/client/app.js
@@ -5,13 +5,18 @@ var shortlyApp = angular.module("shortlyApp", ['ngRoute'])
       controller: 'LinksController',
       templateUrl: 'client/templates/home.html'
     })
+    .when('/create', {
+      controller: 'CreateLinkController',
+      templateUrl: 'client/templates/createLink.html'
+    })
     .otherwise({
       redirectTo: '/'
     });
 })
-.controller('FrameController', function($scope, $http){
+.controller('FrameController', function($scope, $http, $location){
 
 })
+
 .controller('LinksController', function($scope, $http){
   $http({
     method: 'GET',
@@ -21,25 +26,54 @@ var shortlyApp = angular.module("shortlyApp", ['ngRoute'])
     $scope.links = result.data;
     _.each($scope.links, function(link){
       link.sortingTitle = link.title.toLowerCase().trim();
+      if(link.lastclicked === null) {
+        link.lastclicked = -1;
+      }
     });
   });
 
   $scope.displayOptions = {
     reverse: true,
-    orderBy: 'sortingTitle'
-  };
-
-  $scope.sayHello = function(){
-    // alert('hello');
-    $scope.displayOptions.reverse = !$scope.displayOptions.reverse;
-    // debugger;
+    orderBy: 'visits'
   };
 
   $scope.getLastClicked = function(lastclicked){
-    if(lastclicked !== null){
+    if(lastclicked !== -1){
       return new Date(Number(lastclicked) * 1000).toLocaleString();
     } else {
       return 'not visited';
     }
   };
+})
+
+.controller('CreateLinkController', function($scope, $http){
+  $scope.linkText = "";
+
+  $scope.shortenUrl = function(){
+      $http({
+        method: 'POST',
+        url: '/links',
+        data: {url: $scope.linkText}
+      })
+      .success(function(data, status, headers, config){
+        console.log('posted new link');
+      })
+      .error(function(data, status, headers, config){
+        console.log('error posting link');
+      });
+
+
+      // var link = new Shortly.Link( {url: $scope.linkText} );
+      // link.save();
+      $scope.linkText = "";
+      // link.on('request', this.startSpinner, this);
+      // link.on('sync',    this.success,      this );
+      // link.on('error',   this.failure,      this );
+
+    };
 });
+
+
+
+
+
